b010f9a967515e5f5d9080c683ccc62fd5fa875b
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-11-18 14:08:18 -0800

    add progress

--- thread_worker.py
+++ thread_worker.py
@@ -5,6 +5,7 @@ from PySide6.QtCore import QObject, Signal, Slot
 class Worker(QObject):
     finished = Signal(str)
     error = Signal(str)
+    progress = Signal(str)
 
     def __init__(self, fn, *args, **kwargs):
         super().__init__()
@@ -15,10 +16,10 @@ class Worker(QObject):
     @Slot()
     def run(self):
         try:
-            result = self.fn(*self.args, **self.kw)
-            # Ensure result is string
-            if result is None:
-                result = ""
-            self.finished.emit(str(result))
+            result = self.fn(*self.args, progress_callback=self.emit_progress, **self.kw)
+            self.finished.emit(result)
         except Exception as e:
             self.error.emit(str(e))
+    def emit_progress(self, message):
+        # Callback function that scanning functions can call to report progress
+        self.progress.emit(message)
